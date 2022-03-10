8df12286f1ce88b20fb1fd4be09401f333b79ab7
Bianca <bianca.fraccaroli@bandtec.com.br>
2022-03-10 12:30:26 -0300

    Add login endpoint

--- src/main/java/balancefy/api/controller/BalancefyController.java
+++ src/main/java/balancefy/api/controller/BalancefyController.java
@@ -1,9 +1,37 @@
 package balancefy.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import balancefy.api.entities.Usuario;
+import io.jsonwebtoken.Jwts;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.crypto.SecretKey;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/usuarios")
 public class BalancefyController {
+    private List<Usuario> usuarios = new ArrayList();
+
+    @PostMapping
+    private ResponseEntity cadastrar(@RequestBody Usuario usuario) {
+        usuarios.add(usuario);
+
+        return ResponseEntity.status(201).build();
+    }
+
+    @PostMapping("/autenticar/{email}/{senha}")
+    private ResponseEntity login(@PathVariable String email, @PathVariable String senha) {
+        String jwtToken = "";
+        long autenticado = usuarios.stream().filter( usuario -> usuario.autenticar(email, senha)).count();
+
+        if(autenticado != 0) {
+            jwtToken = Jwts.builder().
+                    .compact();
+        }
+
+        return ResponseEntity.status(autenticado == 0 ? 401 : 200).body(jwtToken);
+    }
 
 }
---
+++ src/main/java/balancefy/api/entities/Usuario.java
@@ -0,0 +1,51 @@
+package balancefy.api.entities;
+
+import java.time.LocalDate;
+
+public class Usuario {
+    private int idUsuario;
+    private String nome;
+    private String email;
+    private String senha;
+    private LocalDate dataNascimento;
+
+    public boolean autenticar(String email, String senha) {
+        return email.equals(this.email) && senha.equals(this.senha);
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
